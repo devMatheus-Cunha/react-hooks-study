ddd513d2e1e672aa23b440ff9b99798a5cca9493
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-28 11:58:59 -0300

    feat: useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './App.css'
 
 // hooks
 import UseState from './hooks/UseState';
--- src/hooks/UseState.tsx
+++ src/hooks/UseState.tsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function UseState() {
+  // state
+  const [counter, setCounter] = useState(0)
+
+  //! way one to be made
+  function handlePlusOne(){
+    setTimeout(() => {
+      setCounter(counter + 1)
+    }, 2000);
+  }
+  function handleMinusOne(){
+    setTimeout(() => {
+      setCounter(counter - 1)
+    }, 2000);
+  }
+  
+  //! way two to be done
+  //this way is more useful because if you click twice quickly it will count the two clicks unlike the way above 
+  function handlePlusTwo(){
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1)
+    }, 2000);
+  }
+
+  function handleMinusTwo(){
+    setTimeout(() => {
+      setCounter((prevState) => prevState + 1)
+    }, 2000);
+  }
+
+
   return (
     <div>
-      Olá 
+      <h1>{counter}</h1>
+      {/* <button onClick={handlePlusOne}>+</button> */}
+      {/* <button onClick={handleMinusOne}>-</button> */}
+      <button onClick={handlePlusTwo}>+</button>
+      <button onClick={handleMinusTwo}>-</button>
     </div>
   )
 }
