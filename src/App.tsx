1972fd7d9bb397db4a2fca5a92ea8f8c34c4ab53
Matheus Gomes <gomesprogramacao@gmail.com>
2021-07-30 11:45:12 -0300

    fix: corretion basic style

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,16 @@ import React from "react";
 import "./App.css";
 
 // hooks
-import { UseState } from "./hooks/UseState";
-import { UseEffect } from "./hooks/UseEffect";
+import UseState from "./hooks/UseState";
+import UseEffect from "./hooks/UseEffect";
+import UseContext from "./hooks/UseContext";
 
 function App() {
   return (
     <>
       {/* <UseState /> */}
-      <UseEffect />
+      {/* <UseEffect /> */}
+      <UseContext />
     </>
   );
 }
