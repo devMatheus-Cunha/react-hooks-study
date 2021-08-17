0e6b6832fe8a3c6ff4e30017659bae7c2a438ffb
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-17 17:56:15 -0300

    feat: exercice one useState

--- src/views/examples/UseState.tsx
+++ src/views/examples/UseState.tsx
@@ -1,15 +1,35 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseState = (props:any) => {
-    return (
-        <div className="UseState">
-            <PageTitle
-                title="Hook UseState"
-                subtitle="Estado em componentes funcionais!"
-            />
+const UseState = (props: any) => {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="UseState">
+      <PageTitle
+        title="Hook UseState"
+        subtitle="Estado em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => setCount(count - 1)}>
+            -1
+          </button>
+          <button className="btn" onClick={() => setCount(count + 1)}>
+            +1
+          </button>
+          <button className="btn" onClick={() => setCount((currect: number) => currect + 1000)}>
+            +1000
+          </button>
         </div>
-    )
-}
+      </div>
 
-export default UseState
+      <SectionTitle title="Exercicio #02" />
+    </div>
+  );
+};
+
+export default UseState;
