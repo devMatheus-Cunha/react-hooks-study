b1e5cc684ba5c9c57202be5ea46e95f87294fd95
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-18 18:52:55 -0300

    feat: useState initial

--- src/views/examples/UseEffect.tsx
+++ src/views/examples/UseEffect.tsx
@@ -1,15 +1,23 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React from "react";
+import { useState } from "react";
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
 
-const UseEffect = (props:any) => {
-    return (
-        <div className="UseEffect">
-            <PageTitle
-                title="Hook UseEffect"
-                subtitle="Permite executar efeitos colaterais em componentes funcionais!"
-            />
-        </div>
-    )
-}
+const UseEffect = (props: any) => {
+    const [text, setText] = useState("Digite")
+  return (
+    <div className="UseEffect">
+      <PageTitle
+        title="Hook UseEffect"
+        subtitle="Permite executar efeitos colaterais em componentes funcionais!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+          <h2>{text}</h2>
+          <input type="text" className="input" onChange={({target: value}: any) => setText(value)}/>
+      </div>
+    </div>
+  );
+};
 
-export default UseEffect
+export default UseEffect;
