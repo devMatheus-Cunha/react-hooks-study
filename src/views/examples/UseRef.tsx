a834baa704d67326680914572316b99b46a6e054
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-19 17:43:08 -0300

    feat: useRef one

--- src/views/examples/UseRef.tsx
+++ src/views/examples/UseRef.tsx
@@ -1,13 +1,38 @@
-import React from 'react'
-import PageTitle from '../../components/layout/PageTitle'
+import React, { useState, useRef, useEffect } from "react";
+import PageTitle from "../../components/layout/PageTitle";
 
-export const UseRef = (props:any) => {
-    return (
-        <div className="UseRef">
-            <PageTitle
-                title="Hook UseRef"
-                subtitle="Retorna um objeto mutável com a propriedade .current!"
-            />
-        </div>
-    )
-}
+import SectionTitle from "../../components/layout/SectionTitle";
+
+export const UseRef = (props: any) => {
+  const [valueOne, setValueOne] = useState("");
+  const count = useRef(0);
+
+  useEffect(() => {
+    count.current = count.current + 1;
+  }, [valueOne]);
+  
+  return (
+    <div className="UseRef">
+      <PageTitle
+        title="Hook UseRef"
+        subtitle="Retorna um objeto mutável com a propriedade .current!"
+      />
+      <SectionTitle title="Exercicio #01" />
+
+      <div className="center">
+        <h1>
+          Valor: <span className="red">{valueOne}</span>
+        </h1>
+        <h1>
+          Contador: <span className="red">{count.current}</span>
+        </h1>
+        <input
+          type="text"
+          className="input"
+          value={valueOne}
+          onChange={(event) => setValueOne(event.target.value)}
+        />
+      </div>
+    </div>
+  );
+};
