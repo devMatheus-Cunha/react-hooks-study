2562e2687274d8eb0a07272304e02066fc047f45
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-19 20:10:10 -0300

    feat: useRef two

--- src/views/examples/UseRef.tsx
+++ src/views/examples/UseRef.tsx
@@ -1,16 +1,26 @@
-import React, { useState, useRef, useEffect } from "react";
+import React, { useState, useRef, useEffect, MutableRefObject } from "react";
 import PageTitle from "../../components/layout/PageTitle";
 
 import SectionTitle from "../../components/layout/SectionTitle";
 
 export const UseRef = (props: any) => {
   const [valueOne, setValueOne] = useState("");
+  const [valueTwo, setValueTwo] = useState("");
+
   const count = useRef(0);
+  const myInputOne = useRef<any>(null);
+  const myInputTwo = useRef<any>(null);
 
   useEffect(() => {
     count.current = count.current + 1;
+    myInputTwo.current.focus()
   }, [valueOne]);
-  
+
+  useEffect(() => {
+    count.current++
+    myInputOne.current.focus()
+  }, [valueTwo]);
+
   return (
     <div className="UseRef">
       <PageTitle
@@ -28,9 +38,21 @@ export const UseRef = (props: any) => {
         </h1>
         <input
           type="text"
+          ref={myInputOne}
           className="input"
           value={valueOne}
           onChange={(event) => setValueOne(event.target.value)}
+        />
+      </div>
+
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <input
+          type="text"
+          className="input"
+          ref={myInputTwo}
+          value={valueTwo}
+          onChange={(event) => setValueTwo(event.target.value)}
         />
       </div>
     </div>
