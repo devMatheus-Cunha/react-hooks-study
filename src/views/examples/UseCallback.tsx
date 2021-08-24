c36b98d668e263ece0faabd6783e52220685216a
Matheus Gomes <gomesprogramacao@gmail.com>
2021-08-24 20:41:46 -0300

    feat: useCallback

--- src/views/examples/UseCallback.tsx
+++ src/views/examples/UseCallback.tsx
@@ -1,17 +1,39 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 
 // components
 import PageTitle from "../../components/layout/PageTitle";
 import SectionTitle from "../../components/layout/SectionTitle";
 
 export const UseCallback = (props: any) => {
+  const [count, setCount] = useState(0);
+
+  const handleInc = useCallback((delta) => {
+    setCount((prevent) => prevent + delta);
+  }, []);
+
+  console.log("render...");
+  
   return (
     <div className="UseCallback">
       <PageTitle
         title="Hook UseCallback"
         subtitle="Retorna uma função memoizada!"
-      /> 
+      />
       <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => handleInc(6)}>
+            +6
+          </button>
+          <button className="btn" onClick={() => handleInc(12)}>
+            +12
+          </button>
+          <button className="btn" onClick={() => handleInc(18)}>
+            +18
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
