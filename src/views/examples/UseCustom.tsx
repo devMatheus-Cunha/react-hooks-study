e88305c125fbebed9ca6b64cd5a3139582f40c48
Matheus Gomes <gomesprogramacao@gmail.com>
2021-09-01 17:51:49 -0300

    feat: my Hooks

--- src/views/examples/UseCustom.tsx
+++ src/views/examples/UseCustom.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React from "react";
 
 // components
-import PageTitle from '../../components/layout/PageTitle'
+import PageTitle from "../../components/layout/PageTitle";
+import SectionTitle from "../../components/layout/SectionTitle";
+
+// Hooks
+import { useCounter } from "../../hooks/useCounter";
 
 export const UseCustom = () => {
-    return (
-        <div className="UseCustom">
-            <PageTitle
-                title="Seu Hook"
-                subtitle="Vamos aprender como criar o nosso próprio Hook!"
-            />
-        </div>
-    )
-}
+  const [count, inc, dec ] = useCounter();
 
+  return (
+    <div className="UseCustom">
+      <PageTitle
+        title="Seu Hook"
+        subtitle="Vamos aprender como criar o nosso próprio Hook!"
+      />
+      <SectionTitle title="Exercicio #01" />
+      <div className="center">
+        <span className="text">{count}</span>
+        <div>
+          <button className="btn" onClick={() => dec()}>
+            -1
+          </button>
+          <button className="btn" onClick={() => inc()}>
+            +1
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
